fd49e8dabfe5b231ddb9d994f93944010e802c3d
Hubert Thieriot <hubert@energyandcleanair.org>
2022-01-25 18:20:23 +0800

    -bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="satemdata",
-    version="0.0.2",
+    version="0.0.3",
     author="CREA Dev Team",
     author_email="hubert@energyandcleanair.org",
     description="A package to retrieve emission and feature data from SatEm related databases.",
