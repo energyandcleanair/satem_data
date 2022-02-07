d1a9226cf7dd87474e0a22d886e3c3375edcb1fb
Hubert Thieriot <hubert@energyandcleanair.org>
2022-02-07 12:25:43 +0800

    -adding crosswind_km within index

--- satemdata/result/__init__.py
+++ satemdata/result/__init__.py
@@ -1,6 +1,6 @@
 DB_SATEM = "satem"
 DB_SATEM_TEST = "satem_test"
-RESULTS_UNIQUE_COLS = ["location_id", "date", "frequency", "method.id"]
+RESULTS_UNIQUE_COLS = ["location_id", "date", "frequency", "method.id", "crosswind_km"]
 RESULTS_COLLECTION = "results"
 DATE_FORMAT = "%Y-%m-%d"
 
