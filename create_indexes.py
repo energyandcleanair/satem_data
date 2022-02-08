8e1152849abb00ba590ff1070f66bc5f85acf7fb
Hubert Thieriot <hubert@energyandcleanair.org>
2022-02-08 22:15:27 +0800

    -new result unique columns

--- create_indexes.py
+++ create_indexes.py
@@ -1,8 +1,8 @@
 import satemdata
 from decouple import config
 import os
-from feature.db import create_feature_index
-from result.db import create_result_index
+from satemdata.feature.db import create_feature_index
+from satemdata.result.db import create_result_index
 
 create_feature_index()
 create_result_index()
--- satemdata/result/__init__.py
+++ satemdata/result/__init__.py
@@ -1,6 +1,6 @@
 DB_SATEM = "satem"
 DB_SATEM_TEST = "satem_test"
-RESULTS_UNIQUE_COLS = ["location_id", "date", "frequency", "method.id", "crosswind_km"]
+RESULTS_UNIQUE_COLS = ["location_id", "date", "frequency", "method.id", "crosswind_km", "wind_m_s_threshold"]
 RESULTS_COLLECTION = "results"
 DATE_FORMAT = "%Y-%m-%d"
 
